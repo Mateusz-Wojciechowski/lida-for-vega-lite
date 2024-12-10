2503d47a45a7cf0b2b1800a65045177d812d7b54
Mateusz Wojciechowski <mateuszwojciechowski@Mac-mini-Mateusz.local>
2024-12-10 20:07:40 +0100

    vizevaluator modified, all changes described in a docstring

--- lida/components/viz/vizevaluator.py
+++ lida/components/viz/vizevaluator.py
@@ -5,21 +5,26 @@ from llmx import TextGenerator, TextGenerationConfig, TextGenerationResponse
 
 from lida.datamodel import Goal
 
+'''
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Reduced the number of evaluation dimensions
+    Modified system_prompt so it assumes we have a valid dataset along with our Vega-Lite chart specification
+    Modified message content in the generate function for Vega-Lite compatibility
+'''
+
 system_prompt = """
 You are a helpful assistant highly skilled in evaluating the quality of a given visualization code by providing a score from 1 (bad) - 10 (good) while providing clear rationale. YOU MUST CONSIDER VISUALIZATION BEST PRACTICES for each evaluation. Specifically, you can carefully evaluate the code across the following dimensions
 - bugs (bugs):  are there bugs, logic errors, syntax error or typos? Are there any reasons why the code may fail to compile? How should it be fixed? If ANY bug exists, the bug score MUST be less than 5.
-- Data transformation (transformation): Is the data transformed appropriately for the visualization type? E.g., is the dataset appropriated filtered, aggregated, or grouped  if needed?
-- Goal compliance (compliance): how well the code meets the specified visualization goals?
 - Visualization type (type): CONSIDERING BEST PRACTICES, is the visualization type appropriate for the data and intent? Is there a visualization type that would be more effective in conveying insights? If a different visualization type is more appropriate, the score MUST be less than 5.
 - Data encoding (encoding): Is the data encoded appropriately for the visualization type?
 - aesthetics (aesthetics): Are the aesthetics of the visualization appropriate for the visualization type and the data?
 
-You must provide a score for each of the above dimensions.  Assume that data in chart = plot(data) contains a valid dataframe for the dataset. The `plot` function returns a chart (e.g., matplotlib, seaborn etc object).
+You must provide a score for each of the above dimensions.  Assume that we have a valid dataframe with data used for the analysed visualization.
 
 Your OUTPUT MUST BE A VALID JSON LIST OF OBJECTS in the format:
 
 ```[
-{ "dimension":  "bugs",  "score": x , "rationale": " .."}, { "dimension":  "transformation",  "score": x, "rationale": " .."}, { "dimension":  "compliance",  "score": x, "rationale": " .."},{ "dimension":  "type",  "score": x, "rationale": " .."}, { "dimension":  "encoding",  "score": x, "rationale": " .."}, { "dimension":  "aesthetics",  "score": x, "rationale": " .."}
+{ "dimension":  "bugs",  "score": x , "rationale": " .."}, { "dimension":  "type",  "score": x, "rationale": " .."}, { "dimension":  "encoding",  "score": x, "rationale": " .."}, { "dimension":  "aesthetics",  "score": x, "rationale": " .."}
 ]
 ```
 """
@@ -40,7 +45,7 @@ class VizEvaluator(object):
         messages = [
             {"role": "system", "content": system_prompt},
             {"role": "assistant",
-             "content": f"Generate an evaluation given the goal and code below in {library}. The specified goal is \n\n {goal.question} \n\n and the visualization code is \n\n {code} \n\n. Now, evaluate the code based on the 6 dimensions above. \n. THE SCORE YOU ASSIGN MUST BE MEANINGFUL AND BACKED BY CLEAR RATIONALE. A SCORE OF 1 IS POOR AND A SCORE OF 10 IS VERY GOOD. The structured evaluation is below ."},
+             "content": f"Generate an evaluation given the goal and code below in Vega Lite. The visualization code is \n\n {code} \n\n. Now, evaluate the code based on the 4 dimensions above. \n. THE SCORE YOU ASSIGN MUST BE MEANINGFUL AND BACKED BY CLEAR RATIONALE. A SCORE OF 1 IS POOR AND A SCORE OF 10 IS VERY GOOD. The structured evaluation is below ."},
         ]
 
         # print(messages)
